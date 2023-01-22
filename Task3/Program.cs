8f813d36e485b164202b1a4d7d315aa9cbad1f1d
Yuliya <i@yzubkova.ru>
2023-01-22 21:43:10 +0300

    End

---
+++ Task3/Program.cs
@@ -0,0 +1,46 @@
+﻿// Напишите программу которая принимает на вход координаты двух точек 
+// и находит расстояние между ними в 2Д пространстве
+// (48я минута семинара)
+
+int GetNumber(string message)
+{
+    int result = 0;
+    bool isCorrect = false;
+
+    while(!isCorrect)
+    {
+        Console.WriteLine(message);
+
+        if(int.TryParse(Console.ReadLine(), out result) && result > 0 && result <= 4)
+        {
+            isCorrect = true;
+        }
+        else 
+        {
+            Console.WriteLine("Введите корректное число от 1 до 4.");
+        }
+    }
+    return result;
+}
+
+// получение диапазона координат точек, которые могут лежать в заданной четверти
+string GetDiapazon(int quarter)
+{
+    if(quarter == 1)
+        return "Возможный диапазон координат X и Y от 0 до + бесконечности";
+    else if(quarter == 2)
+        return "Возможный диапазон координат X от 0 до - бесконечности, Y от 0 до + бесконечности";
+    else if(quarter == 3)
+        return "Возможный диапазон координат X от 0 до - бесконечности, Y от 0 до - бесконечности";
+    else
+        return "Возможный диапазон координат X от 0 до + бесконечности, Y от 0 до - бесконечности";
+}
+
+int quarter = GetNumber("Введите номер четверти ");
+
+string diapazon = GetDiapazon(quarter);
+
+Console.WriteLine($"Точка из четверти ({quarter}) имеет координаты в следующем диапазоне ({diapazon})");
+
+
+
