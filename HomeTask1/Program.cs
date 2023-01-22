226b6fa689bfb3b99cb1fe6d2a0ca4bdb06b54b0
Yuliya <i@yzubkova.ru>
2023-01-23 01:12:53 +0300

    Подумать над этой задачей позже

--- HomeTask1/Program.cs
+++ HomeTask1/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Напишите программу, которая принимает на вход пятизначное число и проверяет, 
+является ли оно палиндромом.
+
+14212 -> нет
+
+12821 -> да
+
+23432 -> да
+*/ 
+
+// функция получения числа с консоли
+int GetNumber(string message)
+{
+    int result = 0;
+    bool isCorrect = false;
+
+    while(!isCorrect)
+    {
+        Console.WriteLine(message);
+
+        if(int.TryParse(Console.ReadLine(), out result) && result != 0)
+        {
+            isCorrect = true;
+        }
+        else 
+        {
+            Console.WriteLine("Ввели не число или 0ю Введите корректное число.");
+        }
+    }
+    return result;
+}
+
+int N = GetNumber("Введите пятизначное число: ");
+
+Console.WriteLine(Count(N));
